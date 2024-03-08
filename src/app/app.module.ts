d23b5cbe2d551a19c6e3802473b6d853f0ae684f
Martín Alejandro Estrada Saavedra <martin996@hotmail.com.ar>
2024-03-08 16:07:04 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TooltipModule.forRoot()
   ],
   providers: [
     provideClientHydration()
