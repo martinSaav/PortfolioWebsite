6dc653014f43960574ff44fa9fcbeeedcd070451
Martín Alejandro Estrada Saavedra <martin996@hotmail.com.ar>
2024-03-19 11:19:56 -0300

    a

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -8,7 +8,16 @@ import { Title } from '@angular/platform-browser';
 })
 export class ResumeComponent {
   
-  constructor(@Inject(Title) private titleService: Title) {
+  constructor(@Inject(Title) private titleService: Title, @Inject(Renderer2) private renderer: Renderer2) {
     this.titleService.setTitle('Martin Estrada - Resume');
+  }
+
+  downloadFile(): void {
+    let link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'assets/pdf/CV_martin_estrada.pdf');
+    link.setAttribute('download', 'CV_martin_estrada.pdf');
+    link.click();
+    link.remove();
   }
 }
