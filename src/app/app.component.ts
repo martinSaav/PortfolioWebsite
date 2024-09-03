462f08bb939b05b10751c36352991922a471e353
Martín Alejandro Estrada Saavedra <martin996@hotmail.com.ar>
2024-09-03 11:46:12 -0300

    animacion de scrolleo arriba y abajo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
   title = 'PortfolioWebsite';
+
+  constructor(private el: ElementRef) {}
+
+
+  ngAfterViewInit(): void {
+     const elements = this.el.nativeElement.querySelectorAll('.animate');
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('in-view');
+        } else {
+          entry.target.classList.remove('in-view');
+        }
+      });
+    });
+
+    elements.forEach((element: Element) => {
+      observer.observe(element);
+    });
+  }
+  
 }
